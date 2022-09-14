a5d34ef4b1a330b0caa83e0dc12f754c546a8b3f
Itahand Naizir <itahand.naizir@gmail.com>
2022-09-14 09:28:44 -0500

    Kovan etherscan depricated. Deployed to Goerli.

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.10",
   networks: {
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
